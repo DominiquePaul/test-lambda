4db8f32ccf4f9cf2742276a7091c1a3fc1565de1
Dominique Paul <dominique.c.a.paul@gmail.com>
2024-04-11 13:01:24 +0200

    add ci/cd for lambda test

--- lambda_functions/sendSlackMessageWhenNewImageIsUploaded.py
+++ lambda_functions/sendSlackMessageWhenNewImageIsUploaded.py
@@ -5,5 +5,5 @@ def lambda_handler(event, context):
     print(json.dumps(event))  # For debugging: to see the event structure
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! v3')
+        'body': json.dumps('Hello from Lambda! v4')
     }
