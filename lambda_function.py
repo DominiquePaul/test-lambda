2e92d476acf60623d96d5461d00623b569542d94
Dominique Paul <dominique.c.a.paul@gmail.com>
2024-04-13 12:04:12 +0200

    test inference package with roboflow and predict

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,16 @@
 import os
 import json
-from inference_sdk import InferenceHTTPClient
+# from inference_sdk import InferenceHTTPClient
+from roboflow import Roboflow
 
 
 def handler(event, context):
+    rf = Roboflow(api_key=os.environ["ROBOFLOW_API_KEY"])
+    project = rf.workspace().project("pizza-identifier")
+    model = project.version(3).model
+
+    # infer on a local image
+    return model.predict("pizza.jpg", confidence=40, overlap=30).json()
     client = InferenceHTTPClient(api_url="https://detect.roboflow.com",
                                  api_key=os.environ["ROBOFLOW_API_KEY"])
     img_path = "./pizza.jpg"
