a19d72e050db68f82b5a7e0d169cc9bd35144ab4
Dominique Paul <dominique.c.a.paul@gmail.com>
2024-04-13 11:32:33 +0200

    test inference package with roboflow and predict

--- lambda_function.py
+++ lambda_function.py
@@ -1,15 +1,14 @@
+import os
 import json
-import boto3
-import numpy
-import cv2
+from inference_sdk import InferenceHTTPClient
 
 
 def handler(event, context):
+    client = InferenceHTTPClient(api_url="https://detect.roboflow.com",
+                                 api_key=os.environ["ROBOFLOW_API_KEY"])
+    url = "https://i.imgur.com/JIeYlTI.jpeg"
+    return client.infer(url, model_id="pizza-identifier/3")
     print("Wink emoji")
-    print("Loooool hello")
-    print(dir(boto3))
-    print(dir(numpy))
-    print(dir(cv2))
 
     print(json.dumps(event))  # For debugging: to see the event structure
     return {
